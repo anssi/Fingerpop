e3690d2b79f99c893e4c4e3997e6ccb0c3e48d62
unknown <Anssi@.(none)>
2011-02-22 18:40:24 +0200

    Navigation rework.

--- js/fingerpop.js
+++ js/fingerpop.js
@@ -2,32 +2,63 @@ var stripUrl = "http://www.hs.fi/fingerpori/";
 
 function updateView(url) {
 	$.get(url, function(data) {
-		parseData(data)
+		parseData(data);
 	});
 }
 
 function parseData(data) {
-	var stripUrl = $("#VW_content .strip img", data).attr("src");
+	var stripImageSrc = $("#VW_content .strip img", data).attr("src");
 	var stripDate = $("#VW_content div p", data).text();
-	$("#header").html("Fingerpori " + stripDate + getNav(data));
-	$("#strip").attr("src", stripUrl);
+	$("#header").html("Fingerpori " + stripDate);
+	updateStripImage(stripImageSrc);
+	updateNavigation(data);
 }
 
-function getNav(data) {
-	var nav = "";
+function updateStripImage(stripImageSrc) {
+	$("#strip img").fadeOut(150, function () {
+		$(this).remove();
+	});
+	var stripImage = new Image();
+	$(stripImage).load(function () {
+		$(this).hide();
+		$(this).offset($("#strip").offset());
+		$("#strip").height(stripImage.height);
+		$("#strip").width(stripImage.width);
+		$("#strip").append(this);
+		$(this).fadeIn(150);
+	})
+	.attr("src", stripImageSrc);
+}
+
+function updateNavigation(data) {
 	var previous = $("#VW_content div.previous a", data).attr("href");
 	var next = $("#VW_content div.next a", data).attr("href");
 	
 	if (previous) {
-		nav = "<img onclick=\"updateView('" + previous + "')\" src=\"img/left.png\" />";
+		$("#nav_prev").show();
+		$("#nav_prev").unbind();
+		$("#nav_prev").click(function () {
+			updateView(previous);
+		});
+	} else {
+		$("#nav_prev").hide();
 	}
+	
 	if (next) {
-		nav += "<img onclick=\"updateView('" + next + "')\" src=\"img/right.png\" />";
+		$("#nav_next").show();
+		$("#nav_next").unbind();
+		$("#nav_next").click(function () {
+			updateView(next);
+		});
+	} else {
+		$("#nav_next").hide();
 	}
-	return nav;
 }
 
 $(function() {
+	$(document).bind('mousedown.disableTextSelect', function() {
+		return false;
+	});
 	$("#strip").click(function() {
 		chrome.tabs.create({url: "http://www.hs.fi/fingerpori"});
 	});
