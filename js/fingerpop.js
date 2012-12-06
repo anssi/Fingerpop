7b33c5421834846c5e745b5d17d9d877f413b8cc
Anssi <anssi.asunmaa@gmail.com>
2012-12-06 22:07:24 +0200

    Fixed strip fetching. Updated manifest. Added navigation with keyboard.

--- js/fingerpop.js
+++ js/fingerpop.js
@@ -1,11 +1,22 @@
 var stripUrl = "http://www.hs.fi/fingerpori";
 var animationSpeed = 100;
+var LEFT_ARROW = 37;
+var RIGHT_ARROW = 39;
+
+var previousButton = ""
+var nextButton = ""
+var previousUrl = "";
+var nextUrl = "";
 
 function updateView(url) {
 	$("#strip").addClass("loading");
 	$("#strip img").fadeOut(animationSpeed, function () {
 		$(this).remove();
 	});
+	
+	if (url.indexOf("http://www.hs.fi") != 0) {
+		url = "http://www.hs.fi" + url;
+	}
 	var jqxhr = $.get(url, function(data) {
 		parseData(data);
 	})
@@ -13,8 +24,14 @@ function updateView(url) {
 }
 
 function parseData(data) {
-	var stripImageSrc = $("#VW_content .strip img", data).attr("src");
-	var stripDate = $("#VW_content div p", data).text();
+	var stripImageSrc = ""
+	$("#full-comic div img", data).each(function () {
+		var imageUrl = $(this).attr("src");
+		if (imageUrl.indexOf("sarjis") > 0) {
+			stripImageSrc = imageUrl;
+		}
+	});
+	var stripDate = $("#full-comic .comic-date", data).text();
 	$("#header").html("Fingerpori " + stripDate);
 	updateStripImage(stripImageSrc);
 	updateNavigation(data);
@@ -39,24 +56,37 @@ function updateStripImage(stripImageSrc) {
 }
 
 function updateNavigation(data) {
-	var previous = $("#VW_content div.previous a", data).attr("href");
-	var next = $("#VW_content div.next a", data).attr("href");
+	previousButton = $(".comic-nav a.prev-cm", data);
+	nextButton = $(".comic-nav a.next-cm", data)
+	previousUrl = "";
+	nextUrl = "";
+	if (!previousButton.hasClass("prev-cm-disabled")) {
+		previousUrl = previousButton.attr("href");
+	} else {
+		previousUrl = "";
+	}
 	
-	if (previous) {
+	if (!nextButton.hasClass("next-cm-disabled")) {
+		nextUrl = nextButton.attr("href");	
+	} else {
+		nextUrl = "";
+	}
+	
+	if (previousUrl.length > 0) {
 		$("#nav_prev").show();
 		$("#nav_prev").unbind();
 		$("#nav_prev").click(function () {
-			updateView(previous);
+			updateView(previousUrl);
 		});
 	} else {
 		$("#nav_prev").hide();
 	}
 	
-	if (next) {
+	if (nextUrl.length > 0) {
 		$("#nav_next").show();
 		$("#nav_next").unbind();
 		$("#nav_next").click(function () {
-			updateView(next);
+			updateView(nextUrl);
 		});
 	} else {
 		$("#nav_next").hide();
@@ -64,11 +94,21 @@ function updateNavigation(data) {
 }
 
 $(function() {
+
 	$(document).bind('mousedown.disableTextSelect', function() {
 		return false;
 	});
+	
+	$(document).keyup(function (event) {
+		if (event.keyCode === LEFT_ARROW && previousUrl.length > 0) {
+			updateView(previousUrl);
+		} else if (event.keyCode === RIGHT_ARROW && nextUrl.length > 0) {
+			updateView(nextUrl);
+		}
+	});
+	
 	$("#strip").click(function() {
 		chrome.tabs.create({url: "http://www.hs.fi/fingerpori"});
 	});
-	setTimeout("updateView(stripUrl);", 1);
+	setTimeout(function () {updateView(stripUrl);}, 1);
 });
